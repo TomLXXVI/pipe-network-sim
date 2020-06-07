77eca0e5575905b034bdface76551f29cf1d135e
Tom Christiaens <tom.chr@proximus.be>
2020-06-07 21:48:27 +0200

    modified requirements

--- project/notebooks/dependencies.py
+++ project/notebooks/dependencies.py
@@ -1,7 +1,6 @@
 import sys
 
 sys.path.extend([
-    "D:/python/projecten/jupyter-pypeflow",
-    "D:/python/projecten/jupyter-pypeflow/lib",
-    "D:/python/projecten/jupyter-pypeflow/project",
+    "../../lib",
+    "../../project",
 ])
